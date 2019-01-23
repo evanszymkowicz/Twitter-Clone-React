3ddd69aa1830236287069dab2010982be442f847
Evan Szymkowicz <evanszymkowicz@users.noreply.github.com>
2019-01-22 23:54:45 -0500

    posting and deleting

--- server/models/message.js
+++ server/models/message.js
@@ -2,22 +2,27 @@ const mongoose = require('mongoose');
 const User = require('./user');
 
 //Messages will only be posted when they are tied to a specific user
-const messageSchema = new mongoose.Schema({
-	text: {
-		type: String,
-		required: true,
-		maxLength: 280
+const messageSchema = new mongoose.Schema(
+	{
+		text: {
+			type: String,
+			required: true,
+			maxLength: 280
+		},
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User' //has to match model name
+		}
 	},
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User' //has to match model name
+	{
+		timestamps: true
 	}
-});
+);
 
 messageSchema.pre('remove', async function(next){
 	try {
 		//find user
-		let user = await User.findById(this.userId);
+		let user = await User.findById(this.user);
 		//remove id of the message from the user's list
 		user.message.remove(this.id);
 		//save
@@ -25,7 +30,7 @@ messageSchema.pre('remove', async function(next){
 		//return next
 		return next();
 	} catch (e) {
-			return next(err);
+		return next(err);
 	}
 });
 
